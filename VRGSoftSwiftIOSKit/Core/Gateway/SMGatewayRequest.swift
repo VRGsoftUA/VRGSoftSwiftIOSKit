3a64339436b8be862d45949deed5c017784fd4a9
OLEKSANDR SEMENIUK <semenag01@meta.ua>
2018-08-21 17:24:49 +0300

    Fixed gateway request

--- VRGSoftSwiftIOSKit/Core/Gateway/SMGatewayRequest.swift
+++ VRGSoftSwiftIOSKit/Core/Gateway/SMGatewayRequest.swift
@@ -20,19 +20,8 @@ open class SMGatewayRequest: SMRequest
     open var dataRequest: DataRequest?
     
     open var path: String?
-    open var type: HTTPMethod {
-        didSet {
-            switch type
-            {
-            case .options, .head, .get, .delete:
-                parameterEncoding = URLEncoding.default
-            case .patch, .post, .put:
-                parameterEncoding = JSONEncoding.default
-            default: break
-            }
-        }
-    }
-    open var parameterEncoding: ParameterEncoding = JSONEncoding.default
+    open var type: HTTPMethod
+    open var parameterEncoding: ParameterEncoding?
     
     open var parameters: [String: AnyObject] = [:]
     open var headers: [String: String] = [:]
@@ -44,7 +33,7 @@ open class SMGatewayRequest: SMRequest
     public required init(gateway aGateway: SMGateway, type aType: HTTPMethod)
     {
         gateway = aGateway
-        type = aType
+        self.type = aType
     }
     
     override open func start()
@@ -114,48 +103,72 @@ open class SMGatewayRequest: SMRequest
             allHeaders.updateValue(value, forKey: key)
         }
         
+        switch type
+        {
+        case .options, .head, .get, .delete:
+            parameterEncoding = URLEncoding.default
+        case .patch, .post, .put:
+            parameterEncoding = JSONEncoding.default
+        default: break
+        }
+        
         print("\n\nSTART", self)
         print("URL - ", fullPath, "\n", "TYPE - ", type, "\n", "HEADERS - ", allHeaders, "\n", "PARAMS - ", allParams, "\n\n")
         
-        let dataRequest: DataRequest = Alamofire.request(fullPath, method: type, parameters: allParams, encoding: parameterEncoding, headers: allHeaders)
-        self.dataRequest = dataRequest
-        
-        dataRequest.responseJSON(completionHandler: {[weak self] responseObject in // swiftlint:disable:this explicit_type_interface
-            
-            switch responseObject.result
+        if parameterEncoding == nil
+        {
+            switch type
             {
-            case .success:
-                //                    print("Request success with data: \(data)")
-                let callBack: SMRequestParserBlock = { (aResponse: SMResponse) in
-                    if let strongSelf: SMGatewayRequest = self
-                    {
-                        if strongSelf.executeAllResponseBlocksSync
+            case .options, .head, .get, .delete:
+                parameterEncoding = URLEncoding.default
+            case .patch, .post, .put:
+                parameterEncoding = JSONEncoding.default
+            default: break
+            }
+        }
+        
+        if let parameterEncoding: ParameterEncoding = parameterEncoding
+        {
+            let dataRequest: DataRequest = Alamofire.request(fullPath, method: type, parameters: allParams, encoding: parameterEncoding, headers: allHeaders)
+            self.dataRequest = dataRequest
+            
+            dataRequest.responseJSON(completionHandler: {[weak self] responseObject in // swiftlint:disable:this explicit_type_interface
+                
+                switch responseObject.result
+                {
+                case .success:
+                    //                    print("Request success with data: \(data)")
+                    let callBack: SMRequestParserBlock = { (aResponse: SMResponse) in
+                        if let strongSelf: SMGatewayRequest = self
                         {
-                            strongSelf.executeSynchronouslyAllResponseBlocks(response: aResponse)
-                        } else
-                        {
-                            strongSelf.executeAllResponseBlocks(response: aResponse)
+                            if strongSelf.executeAllResponseBlocksSync
+                            {
+                                strongSelf.executeSynchronouslyAllResponseBlocks(response: aResponse)
+                            } else
+                            {
+                                strongSelf.executeAllResponseBlocks(response: aResponse)
+                            }
                         }
                     }
-                }
-                
-                if let successParserBlock: SMGatewayRequestSuccessParserBlock = self?.successParserBlock
-                {
-                    successParserBlock(dataRequest, responseObject, callBack)
-                } else
-                {
-                    if let response: SMResponse = self?.successBlock?(dataRequest, responseObject)
+                    
+                    if let successParserBlock: SMGatewayRequestSuccessParserBlock = self?.successParserBlock
                     {
-                        callBack(response)
+                        successParserBlock(dataRequest, responseObject, callBack)
+                    } else
+                    {
+                        if let response: SMResponse = self?.successBlock?(dataRequest, responseObject)
+                        {
+                            callBack(response)
+                        }
                     }
+                case .failure(let error): // swiftlint:disable:this explicit_type_interface
+                    print("Request failed with error: \(error)")
+                    self?.executeFailureBlock(responseObject: responseObject)
                 }
-            case .failure(let error): // swiftlint:disable:this explicit_type_interface
-                print("Request failed with error: \(error)")
-                self?.executeFailureBlock(responseObject: responseObject)
-            }
-        })
-        
-        return completion(dataRequest)
+            })
+            
+            return completion(dataRequest)
+        }
     }
     
     open func executeSuccessBlock(responseObject aResponseObject: DataResponse<Any>)
