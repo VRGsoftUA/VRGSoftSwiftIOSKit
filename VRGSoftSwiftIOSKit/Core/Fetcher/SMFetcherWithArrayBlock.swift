f7fc47b35dff09ef7515aeed60a144dae9fbb68c
kudlay <kudlay8883@gmail.com>
2020-02-12 13:47:59 +0200

    added SMFetcherWithArrayBlock

---
+++ VRGSoftSwiftIOSKit/Core/Fetcher/SMFetcherWithArrayBlock.swift
@@ -0,0 +1,46 @@
+//
+//  SMFetcherWithArrayBlock.swift
+//  VRGSoftSwiftIOSKit
+//
+//  Created by OLEKSANDR SEMENIUK on 3/12/19.
+//  Copyright © 2019 VRG Soft. All rights reserved.
+//
+
+import Foundation
+
+public typealias SMDataArrayFetchBlock = () -> [Any]?
+
+open class SMFetcherWithArrayBlock: SMDataFetcherProtocol {
+    
+    // MARK: SMDataFetcherProtocol
+    
+    public var callbackQueue: DispatchQueue = DispatchQueue.global()
+    
+    public let fetchBlock: SMDataArrayFetchBlock
+    
+    
+    public init(fetchBlock aFetchBlock: @escaping SMDataArrayFetchBlock) {
+        
+        self.fetchBlock = aFetchBlock
+    }
+    
+    open func canFetchWith(message aMessage: SMFetcherMessage) -> Bool {
+        
+        return true
+    }
+    
+    public func fetchDataBy(message aMessage: SMFetcherMessage, withCallback aFetchCallback: @escaping SMDataFetchCallback) {
+        
+        self.callbackQueue.async {
+            
+            let response: SMResponse = SMResponse()
+            response.isSuccess = true
+            response.boArray = self.fetchBlock() as [AnyObject]? ?? []
+            aFetchCallback(response)
+        }
+    }
+    
+    public func cancelFetching() {
+        
+    }
+}
