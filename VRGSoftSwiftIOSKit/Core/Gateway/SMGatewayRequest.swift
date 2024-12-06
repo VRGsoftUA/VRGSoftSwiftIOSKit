aa5d6b7b35953fc472a78d59808b6a649866aea1
Oleksii Kudlai <kudlay8883@gmail.com>
2024-12-06 09:41:47 +0200

    add timeoutInterval

--- VRGSoftSwiftIOSKit/Core/Gateway/SMGatewayRequest.swift
+++ VRGSoftSwiftIOSKit/Core/Gateway/SMGatewayRequest.swift
@@ -33,7 +33,8 @@ open class SMGatewayRequest: SMRequest {
     
     open var retryCount: Int = 0
     open var retryTime: TimeInterval = 0.5
-    
+    open var timeoutInterval: TimeInterval = 60
+
     open var path: String?
     open var type: HTTPMethod
     open var parameterEncoding: ParameterEncoding?
@@ -154,7 +155,7 @@ open class SMGatewayRequest: SMRequest {
                                                       parameters: allParams,
                                                       encoding: parameterEncoding,
                                                       headers: allHeaders,
-                                                      interceptor: SMGatewayConfigurator.shared.interceptor)
+                                                      interceptor: SMGatewayConfigurator.shared.interceptor) { $0.timeoutInterval = self.timeoutInterval }
 
             self.dataRequest = dataRequest
 
